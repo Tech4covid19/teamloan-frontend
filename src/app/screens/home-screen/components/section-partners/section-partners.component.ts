92bcd71e042b7853bf6b194b66750c2050e4300c
João Bordalo <joao.bordalo@gowithflow.io>
2020-04-06 22:12:28 +0100

    [16] Partners logos

--- src/app/screens/home-screen/components/section-partners/section-partners.component.ts
+++ src/app/screens/home-screen/components/section-partners/section-partners.component.ts
@@ -4,8 +4,32 @@ import { THEME } from 'src/app/material/button/button.component';
 @Component({
     selector: 'app-section-partners',
     templateUrl: './section-partners.component.html',
-    styleUrls: ['./section-partners.component.scss'],
+    styleUrls: ['./section-partners.component.scss']
 })
 export class SectionPartnersComponent {
     public buttonTheme = THEME.SECUNDARY;
+
+    public partners = [
+        {
+            href: 'https://madeoflisboa.com',
+            title: `Made of Lisboa | Official Lisbon's Entrepreneur Community`,
+            src: '/assets/img/logos/cmlisboa.svg'
+        },
+        {
+            href: 'http://www.acbraga.pt',
+            title: 'Associação Comercial de Braga',
+            src: '/assets/img/logos/acb.svg'
+        },
+        {
+            href: 'https://www.nestportugal.pt',
+            title: 'NEST - Centro de inovação do turismo',
+            src: '/assets/img/logos/nest.svg'
+        },
+        {
+            href: 'https://www.cap.pt/',
+            title:
+                'CAP - Confederação dos Agricultores de Portugal | Confederação dos Agricultores de Portugal',
+            src: '/assets/img/logos/cap.svg'
+        }
+    ];
 }
