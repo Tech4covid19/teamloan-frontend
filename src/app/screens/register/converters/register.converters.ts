5da5e99607e5b35814d62ad7a69d7e8800b37de6
Fernando Marins <f.abreu.marins@gmail.com>
2020-04-25 20:57:09 +0100

    validating zip code before call api

--- src/app/screens/register/converters/register.converters.ts
+++ src/app/screens/register/converters/register.converters.ts
@@ -13,7 +13,7 @@ export class RegisterConverters {
             email: registerViewModel.user.email,
             vat: registerViewModel.company.nif,
             phone: registerViewModel.user.phone,
-            'zip-code': registerViewModel.company.postalCode,
+            'zip-code': this.uniformZipCodeFormat(registerViewModel.company.postalCode),
             password: registerViewModel.user.password,
             intent: registerViewModel.company.intent
         });
@@ -24,5 +24,15 @@ export class RegisterConverters {
             key: businessArea.uuid,
             label: businessArea.name
         };
+    }
+
+    private static uniformZipCodeFormat(zipCode: string): string {
+        const regexWithDash = /^\d{4}-\d{3}?$/;
+
+        if (regexWithDash.test(zipCode)) {
+            return zipCode;
+        }
+
+        return zipCode.substring(0, 4) + '-' + zipCode.substring(4, 7);
     }
 }
