449a5daad47c39e3520da0596d4f84d12d26fb29
Fernando Marins <f.abreu.marins@gmail.com>
2020-05-07 20:38:25 +0100

    adding confirm password in the register form

--- src/app/screens/register/components/user-form/user-form.service.ts
+++ src/app/screens/register/components/user-form/user-form.service.ts
@@ -4,16 +4,21 @@ import { FormGeneratorService } from 'src/app/form-tools/interfaces/form-generat
 import { PasswordFormatValidator } from 'src/app/form-tools/validators/password-format.validator';
 import { PhoneValidator } from 'src/app/form-tools/validators/phone.validator';
 import { EmailValidator } from 'src/app/form-tools/validators/email.validator';
+import { MatchValidator } from 'src/app/form-tools/validators/match.validator';
 
 @Injectable()
-export class UserFormService implements FormGeneratorService  {
+export class UserFormService implements FormGeneratorService {
     constructor(private fb: FormBuilder) {}
 
     public getForm(): FormGroup {
-        return this.fb.group({
-            phone: ['', Validators.compose( [ Validators.required, PhoneValidator] ) ],
-            email: ['', Validators.compose( [ Validators.required, EmailValidator] ) ],
-            password: ['', Validators.compose([Validators.required, PasswordFormatValidator]) ]
-        });
+        return this.fb.group(
+            {
+                phone: ['', Validators.compose([Validators.required, PhoneValidator])],
+                email: ['', Validators.compose([Validators.required, EmailValidator])],
+                password: ['', Validators.compose([Validators.required, PasswordFormatValidator])],
+                confirmPassword: ['', Validators.required]
+            },
+            { validator: MatchValidator('password', 'confirmPassword') }
+        );
     }
 }
--- src/app/screens/register/register-user.viewmodel.ts
+++ src/app/screens/register/register-user.viewmodel.ts
@@ -17,6 +17,7 @@ export interface UserViewModel {
     phone: string;
     email: string;
     password: string;
+    confirmPassword: string;
 }
 
 export interface RegisterViewModel {
